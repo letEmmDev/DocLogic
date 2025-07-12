fae19eddc5bfc2906ba82b649786cf61d054123d
Saikyyo <ramos.emmanuel@outlook.fr>
2025-07-12 15:21:23 +0200

    Project update - Admin panel implementation for users management + User bulk creation w/ manag.py shell

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'name', 'surname', 'role', 'is_staff', 'is_active')
+    list_filter = ('role', 'is_staff', 'is_active')
+    search_fields = ('email', 'name', 'surname')
+    ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'name', 'surname', 'role')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'surname', 'role', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
