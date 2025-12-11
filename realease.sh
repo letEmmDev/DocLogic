517f763e93745d3a5c5b57e61fae52a9da90c706
Emmanuel <88629360+letEmmDev@users.noreply.github.com>
2025-12-11 18:56:13 +0100

    Project fix - Settings for deployment on Appliku v2.3 | Migration files reintegrations

--- realease.sh
+++ realease.sh
@@ -3,5 +3,9 @@ set -e
 echo "Starting release process..."
 python manage.py migrate
 python manage.py makemigrations
+python manage.py makemigrations
+python manage.py makemigrations
+python manage.py makemigrations
+python manage.py makemigrations
 python manage.py createsuperuser
 
